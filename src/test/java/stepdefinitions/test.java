3e951ae99d069c9bca4d51fa0a5ac9e4a8efd7ff
willmanc-1 <58673845+willmanc-1@users.noreply.github.com>
2020-10-18 16:31:06 -0400

    First commit

---
+++ src/test/java/stepdefinitions/test.java
@@ -0,0 +1,52 @@
+package stepdefinitions;
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import static utils.BrowserFactory.driver;
+
+public class test {
+
+    // Navigating to Google Home Page
+    @Given("I navigate to Google home page")
+    public void Google_homepage() {
+        driver.get("https://www.google.com");
+    }
+
+    //Entering String into Google to Search
+    @When("I search for {string}")
+    public void Google_search_for(String text) {
+        driver.findElement(By.name("q")).sendKeys(text);
+        driver.findElement(By.name("q")).submit();
+    }
+
+    //Returning first link text to confirm search string
+    @Then("The page should contain {string}")
+    public void Google_page_contains(String text) {
+        driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+        driver.findElements(By.tagName("a"));
+        System.out.println(driver.getTitle());
+    }
+
+    //Return top ten website links and info. from search string
+    @Then("I return the top ten website for {string}")
+    public void Google_page_websites(String text) {
+        driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        for (WebElement x : links) {
+            System.out.println(x.getText());
+        }
+    }
+
+    @After()
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
+
+
+
---
+++ src/test/resources/features/test.feature
@@ -0,0 +1,14 @@
+@test
+Feature: Google Search
+This feature verifies Google Searches
+
+   Scenario: Testing that you can enter a search into google and Google will return results
+       Given I navigate to Google home page
+            When I search for "Flooid"
+            Then The page should contain "Flooid"
+
+    Scenario: List 10 top  website that contains the phone number of flooid coventry and validate any website has details other than flooid
+         Given I navigate to Google home page
+               When I search for "Flooid PCMS +44 (0)24 7669 4455"
+               Then I return the top ten website for "Flooid PCMS +44 (0)24 7669 4455"
+      
