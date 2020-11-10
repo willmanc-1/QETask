ee40b1828208e53e9d47453edea55f0d3de3b2f8
Cody Willman <58673845+willmanc-9@users.noreply.github.com>
2020-11-10 08:19:12 -0500

    Update test.java

--- src/test/java/stepdefinitions/test.java
+++ src/test/java/stepdefinitions/test.java
@@ -1,52 +1,64 @@
 package stepdefinitions;
 import io.cucumber.java.After;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 import static utils.BrowserFactory.driver;
 
-public class test {
 
-    // Navigating to Google Home Page
-    @Given("I navigate to Google home page")
-    public void Google_homepage() {
-        driver.get("https://www.google.com");
-    }
+    public class test{
+        // Navigating to Google Home Page
+        @Given("I navigate to Google home page")
+        public void Google_homepage() {
+            driver.get("https://www.google.com");
+        }
 
-    //Entering String into Google to Search
-    @When("I search for {string}")
-    public void Google_search_for(String text) {
-        driver.findElement(By.name("q")).sendKeys(text);
-        driver.findElement(By.name("q")).submit();
-    }
+        // Entering String into Google to Search
+        @When("I search for {string}")
+        public void Google_search(String text) {
+            WebElement x = driver.findElement(By.name("q"));
+            x.sendKeys(text);
+            x.submit();
+        }
 
-    //Print search string to confirm google search
-    @Then("The page should contain {string}")
-    public void Google_page_contains(String text) {
-        driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
-        driver.findElements(By.tagName("a"));
-        System.out.println(driver.getTitle());
-    }
+        // Navigating to flooid
+        @Then("I navigate to {string}")
+        public void google_Navigate(String Text) {
+            driver.navigate().to(Text);
+        }
 
-    //Return top ten website links and info. from search string
-    @Then("I return the top ten website for {string}")
-    public void Google_page_websites(String text) {
-        driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        for (WebElement x : links) {
-            System.out.println(x.getText());
+        // Validating URL
+        @And("the URL is flooid.com")
+        public void url_Validation() {
+            String currentURL = driver.getCurrentUrl();
+            String expectedURL = "https://www.flooid.com/";
+            Assert.assertEquals(currentURL,expectedURL);
+            driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, expectedURL);
+            if (!url.equals(expectedURL)) System.out.println("URL's do not match");
+            System.out.println("URL's match");
+        }
+
+        // Validating Logo
+        @And("I confirm the Flooid Logo is displayed")
+        public void logo_Validation() {
+            String expectedImage = "https://www.flooid.com/static/fc94747ad2c6944914d518ce2ff4d808/a54c6/bg-blob.png";
+            driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+            String image = driver.findElement(By.cssSelector("img")).getAttribute("src");
+            System.out.println(image);
+            Assert.assertEquals(image,expectedImage);
+        }
+        @After()
+        public void closeBrowser() {
+            driver.quit();
         }
     }
-
-    @After()
-    public void closeBrowser() {
-        driver.quit();
-    }
-}
 
 
 
